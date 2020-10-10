b9a58eb76e9e1cd098ec01b304615e4245db04b4
igmrrf <francis.igbiriki.fi@gmail.com>
2020-10-10 04:43:01 +0100

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,46 @@
+require(`dotenv`).config({
+  path: `.env`,
+});
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
+
+module.exports = {
+  siteMetadata: {
+    siteTitleAlt: `IGMRRF - TLDO`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `tldo`,
+        short_name: `tldo`,
+        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        start_url: `/`,
+        background_color: `#141821`,
+        theme_color: `#f6ad55`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+};
